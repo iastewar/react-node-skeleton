c9fb4863a1cfa3bd206d1387c1538a7dd0e88a2a
Ian Stewart <>
2022-01-26 13:32:16 -0700

    added echarts and webconfig for deployment

--- README.md
+++ README.md
@@ -27,6 +27,9 @@ It correctly bundles React in production mode and optimizes the build for the be
 The build is minified and the filenames include the hashes.\
 Your app is ready to be deployed!
 
+It will also copy the web.config to the build server so that it is ready for Azure Web App deployment. You can now
+deploy through vscode with the Azure extension.
+
 See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
 
 ### `npm run eject`
--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,31 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ReactECharts from 'echarts-for-react';
 
 function App() {
+  const option = {
+    xAxis: {
+      data: ['2017-10-24', '2017-10-25', '2017-10-26', '2017-10-27']
+    },
+    yAxis: {},
+    series: [
+      {
+        type: 'candlestick',
+        data: [
+          [20, 34, 10, 38],
+          [40, 35, 30, 50],
+          [31, 38, 33, 44],
+          [38, 15, 5, 42]
+        ]
+      }
+    ]
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div className='main-container'>
+     <ReactECharts option={option}></ReactECharts>
+   </div>
   );
 }
 
